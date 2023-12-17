e14e78c663d1e342a78be3362d4b79928eddcd80
RenatoGSantos <39703265+renatogsantos@users.noreply.github.com>
2023-12-16 21:03:17 -0300

    new: novo projeto

---
+++ src/components/Switch.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Switch } from "@nextui-org/react";
+import { Moon, SunDim } from "@phosphor-icons/react";
+import { useTheme } from "next-themes";
+
+export default function MainSwitch() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  return (
+    <Switch
+      defaultSelected
+      size="md"
+      color="primary"
+      startContent={<SunDim size={24} weight="duotone" />}
+      endContent={<Moon size={24} weight="duotone" />}
+      onChange={(e) => {
+        let tema = e.target.checked ? "light" : "dark";
+        setTheme(tema);
+      }}
+    />
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,70 @@
-import type { Config } from 'tailwindcss'
+// tailwind.config.js
+import { nextui } from "@nextui-org/react";
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config: any = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        blue: {
+          50: "#e6f1fe",
+          100: "#cce3fd",
+          200: "#99c7fb",
+          300: "#66aaf9",
+          400: "#338ef7",
+          500: "#006FEE",
+          600: "#005bc4",
+          700: "#004493",
+          800: "#002e62",
+          900: "#001731",
+        },
       },
     },
   },
-  plugins: [],
-}
-export default config
+  darkMode: "class",
+  plugins: [
+    nextui({
+      prefix: "nextui", // prefix for themes variables
+      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+      defaultTheme: "light", // default theme from the themes object
+      defaultExtendTheme: "light", // default theme to extend on custom themes
+      layout: {}, // common layout tokens (applied to all themes)
+      themes: {
+        light: {
+          layout: {}, // light theme layout tokens
+          colors: {
+            background: "#FFFFFF", // or DEFAULT
+            foreground: "#11181C", // or 50 to 900 DEFAULT
+            primary: {
+              foreground: "#FFFFFF",
+              DEFAULT: "#006FEE",
+            },
+          }, // light theme colors
+        },
+        dark: {
+          layout: {}, // dark theme layout tokens
+          colors: {
+            background: "#000000", // or DEFAULT
+            foreground: "#ECEDEE", // or 50 to 900 DEFAULT
+            primary: {
+              //... 50 to 900
+              foreground: "#FFFFFF",
+              DEFAULT: "#006FEE",
+            },
+          }, // dark theme colors
+        },
+        // ... custom themes
+      },
+    }),
+  ],
+};
+
+export default config;
