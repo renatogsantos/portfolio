30dede430a21b0648b8bef7f3ec6699f43865d70
RenatoGSantos <39703265+renatogsantos@users.noreply.github.com>
2023-12-16 21:12:44 -0300

    feat: ajuste de tema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="">
       <MainNavbar />
       <div className="container mx-auto p-12 rounded-3xl dark:bg-slate-800 bg-slate-200 mt-16">
-        <h1 className="text-8xl font-extrabold">Renato G Santos</h1>
+        <h1 className="text-3xl lg:text-8xl font-extrabold">Renato G Santos</h1>
       </div>
     </main>
   );
