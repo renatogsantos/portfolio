692dcdd5bd15bc756c671ecf057a11ec3ce1edfc
RenatoGSantos <39703265+renatogsantos@users.noreply.github.com>
2023-12-20 20:07:50 -0300

    feat: adiciona link ao title cardportfolio

--- src/components/CardPortfolio.tsx
+++ src/components/CardPortfolio.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { LinkSimple } from "@phosphor-icons/react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 
@@ -15,23 +17,25 @@ export default function CardPortfolio({
   link,
 }: CardProps) {
   return (
-    <Link href={link} target="_blank" aria-label={title}>
-      <motion.div
-        initial={{ scale: 0 }}
-        whileInView={{ scale: 1 }}
-        viewport={{ once: true }}
-        transition={{ duration: 1, bounce: 0.2, type: "spring", delay: 0.5 }}
-        className="flex flex-col gap-1 relative"
-      >
-        <div
-          className="bg-slate-400 rounded-3xl aspect-video card-portfolio"
-          style={{ backgroundImage: `url(${image})` }}
-        />
-        <div className="flex flex-col mt-6">
-          <span className="text-md font-medium">{title}</span>
-          <span className="text-sm">{description}</span>
-        </div>
-      </motion.div>
-    </Link>
+    <motion.div
+      initial={{ scale: 0 }}
+      whileInView={{ scale: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 1, bounce: 0.2, type: "spring", delay: 0.5 }}
+      className="flex flex-col gap-1 relative"
+    >
+      <div
+        className="bg-slate-400 rounded xl:rounded-2xl aspect-video card-portfolio"
+        style={{ backgroundImage: `url(${image})` }}
+      />
+      <div className="flex flex-col mt-6">
+        <Link href={link} target="_blank" aria-label={title}>
+          <span className="text-md font-medium flex">
+            <LinkSimple size={24} /> {title}
+          </span>
+        </Link>
+        <span className="text-sm">{description}</span>
+      </div>
+    </motion.div>
   );
 }
