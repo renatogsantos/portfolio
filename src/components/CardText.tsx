769131d21be8a4b8572b37f7f0a711b5bcf61686
RenatoGSantos <39703265+renatogsantos@users.noreply.github.com>
2023-12-20 19:00:12 -0300

    feat: cria componente de cardtext

---
+++ src/components/CardText.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { motion } from "framer-motion";
+
+type CardProps = {
+  icon: any;
+  title: string;
+  description: string;
+};
+
+export default function CardText({ icon, title, description }: CardProps) {
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 50 }}
+        viewport={{ once: true }}
+        transition={{
+          duration: 1,
+          bounce: 0.6,
+          type: "spring",
+          delay: 0.5,
+        }}
+        className="flex flex-col gap-4 p-6"
+      >
+        <motion.div
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          viewport={{ once: true }}
+          transition={{
+            duration: 1,
+            bounce: 0.6,
+            type: "spring",
+            delay: 0.5,
+          }}
+          className="card-icon"
+        >
+          {icon}
+        </motion.div>
+        <h3 className="text-2xl lg:text-3xl font-bold text-main-700 dark:text-white">
+          {title}
+        </h3>
+        <p className="text-sm">{description}</p>
+      </motion.div>
+    </>
+  );
+}
--- src/components/sections/HeroOne.tsx
+++ src/components/sections/HeroOne.tsx
@@ -14,6 +14,7 @@ import { BezierCurve, ChartLineUp, Code, Flask } from "@phosphor-icons/react";
 import { motion } from "framer-motion";
 import ParallaxText from "../ParallaxText";
 import { Button, Tooltip } from "@nextui-org/react";
+import CardText from "../CardText";
 
 export default function HeroOne() {
   return (
@@ -201,125 +202,52 @@ export default function HeroOne() {
           </motion.div>
           <div className="xl:w-full">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-              <motion.div
-                initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 50 }}
-                viewport={{ once: true }}
-                transition={{
-                  duration: 1,
-                  bounce: 0.6,
-                  type: "spring",
-                  delay: 0.5,
-                }}
-                className="flex flex-col gap-4 p-6"
-              >
-                <div className="card-icon">
-                  <BezierCurve size={40} weight="duotone" />
-                </div>
-                <h3 className="text-2xl lg:text-3xl font-bold text-main-700 dark:text-white">
-                  Design Imersivo para Sua Marca
-                </h3>
-                <p className="text-sm">
-                  Dê vida à sua marca com o Studio Digital! Nossa equipe
+              <CardText
+                icon={<BezierCurve size={40} weight="duotone" />}
+                title="Design Imersivo para Sua Marca"
+                description="Dê vida à sua marca com o Studio Digital! Nossa equipe
                   especializada em Design UX/UI está pronta para transformar
                   suas ideias em uma experiência digital envolvente. Descubra
                   como podemos criar interfaces intuitivas e visualmente
                   cativantes que destacam sua presença online. Explore o
                   potencial ilimitado do design imersivo e destaque-se em meio à
                   concorrência. Confie no Studio Digital para elevar sua marca a
-                  novos patamares!
-                </p>
-              </motion.div>
-
-              <motion.div
-                initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 50 }}
-                viewport={{ once: true }}
-                transition={{
-                  duration: 1,
-                  bounce: 0.6,
-                  type: "spring",
-                  delay: 0.5,
-                }}
-                className="flex flex-col gap-4 p-6"
-              >
-                <div className="card-icon">
-                  <Code size={40} weight="duotone" />
-                </div>
-                <h3 className="text-2xl lg:text-3xl font-bold text-main-700 dark:text-white">
-                  Desenvolvimento Web Avançado
-                </h3>
-                <p className="text-sm">
-                  Explore o futuro da web com o Studio Digital! Nossa equipe de
-                  Desenvolvimento utiliza ferramentas como Next.js, Tailwindcss,
-                  Typescript para criar websites rápidos, responsivos e
-                  inovadores. Descubra como podemos otimizar a performance do
-                  seu site, proporcionando uma experiência de usuário
-                  excepcional. Alcance novos horizontes com o desenvolvimento
-                  web avançado do Studio Digital e posicione-se à frente no
-                  mundo online.
-                </p>
-              </motion.div>
-              <motion.div
-                initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 50 }}
-                viewport={{ once: true }}
-                transition={{
-                  duration: 1,
-                  bounce: 0.6,
-                  type: "spring",
-                  delay: 0.5,
-                }}
-                className="flex flex-col gap-4 p-6"
-              >
-                <div className="card-icon">
-                  <Flask size={40} weight="duotone" />
-                </div>
-                <h3 className="text-2xl lg:text-3xl font-bold text-main-700 dark:text-white">
-                  Transforme Ideias em Experiências Digitais Extraordinárias
-                </h3>
-                <p className="text-sm">
-                  No Studio Digital, suas ideias ganham vida digital!
-                  Permita-nos guiá-lo em uma jornada onde a inovação encontra o
-                  design. Descubra como transformamos conceitos em experiências
-                  digitais extraordinárias, destacando o que há de único na sua
-                  visão. Sua presença online nunca foi tão impactante. Confie no
-                  nosso expertise para transformar suas ideias em realidade
-                  digital.
-                </p>
-              </motion.div>
-              <motion.div
-                initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 50 }}
-                viewport={{ once: true }}
-                transition={{
-                  duration: 1,
-                  bounce: 0.6,
-                  type: "spring",
-                  delay: 0.5,
-                }}
-                className="flex flex-col gap-4 p-6"
-              >
-                <div className="card-icon">
-                  <ChartLineUp size={40} weight="duotone" />
-                </div>
-                <h3 className="text-2xl lg:text-3xl font-bold text-main-700 dark:text-white">
-                  O Caminho para o Sucesso Online Começa Aqui
-                </h3>
-                <p className="text-sm">
-                  No Studio Digital, a excelência é o nosso padrão! Descubra
-                  como podemos elevar a sua presença online, proporcionando
-                  resultados tangíveis. Seja visto, seja lembrado - com
-                  estratégias comprovadas e um toque de criatividade, nossa
-                  equipe está pronta para guiar sua marca rumo ao sucesso
-                  online. Invista no seu futuro digital com o Studio Digital e
-                  alcance novos patamares de visibilidade e impacto.
-                </p>
-              </motion.div>
+                  novos patamares!"
+              />
+              <CardText
+                icon={<Code size={40} weight="duotone" />}
+                title="Desenvolvimento Web Avançado"
+                description="Explore o futuro da web com o Studio Digital! Nossa equipe de
+                Desenvolvimento utiliza ferramentas como Next.js, Tailwindcss,
+                Typescript para criar websites rápidos, responsivos e
+                inovadores. Descubra como podemos otimizar a performance do
+                seu site, proporcionando uma experiência de usuário
+                excepcional. Alcance novos horizontes com o desenvolvimento
+                web avançado do Studio Digital e posicione-se à frente no
+                mundo online."
+              />
+              <CardText
+                icon={<Flask size={40} weight="duotone" />}
+                title="Transforme Ideias em Experiências Digitais Extraordinárias"
+                description="No Studio Digital, suas ideias ganham vida digital!
+                Permita-nos guiá-lo em uma jornada onde a inovação encontra o
+                design. Descubra como transformamos conceitos em experiências
+                digitais extraordinárias, destacando o que há de único na sua
+                visão. Sua presença online nunca foi tão impactante. Confie no
+                nosso expertise para transformar suas ideias em realidade
+                digital."
+              />
+              <CardText
+                icon={<ChartLineUp size={40} weight="duotone" />}
+                title="O Caminho para o Sucesso Online Começa Aqui"
+                description="No Studio Digital, a excelência é o nosso padrão! Descubra
+                como podemos elevar a sua presença online, proporcionando
+                resultados tangíveis. Seja visto, seja lembrado - com
+                estratégias comprovadas e um toque de criatividade, nossa
+                equipe está pronta para guiar sua marca rumo ao sucesso
+                online. Invista no seu futuro digital com o Studio Digital e
+                alcance novos patamares de visibilidade e impacto."
+              />
             </div>
           </div>
         </div>
