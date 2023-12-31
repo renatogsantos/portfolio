a3e1455b1942d15f1248640c8cc7b46f99134d94
RenatoGSantos <39703265+renatogsantos@users.noreply.github.com>
2023-12-31 16:18:48 -0300

    fix: corrige icon button

--- src/components/sections/HomeSection.tsx
+++ src/components/sections/HomeSection.tsx
@@ -41,6 +41,7 @@ export default function HomeSection() {
             Seu sucesso é nossa missão
           </p>
           <MainButton
+            icon={null}
             typeButton="button"
             title="Entre em contato"
             onClick={() => {
